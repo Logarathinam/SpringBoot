520effcdbee578b185a9a5a3815cfab070768d07
Logarathinam A <logarathinam.a@signify.com>
2020-09-05 12:32:48 +0530

    Adding a POST method

--- src/main/java/com/example/controller/TopicsController.java
+++ src/main/java/com/example/controller/TopicsController.java
@@ -1,9 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,4 +23,9 @@ public class TopicsController {
         return topicservice.getTopic(id);
     }
 
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody pojo topic)
+    {
+       topicservice.addTopic(topic);
+    }
 }
--- src/main/java/com/example/controller/TopicsService.java
+++ src/main/java/com/example/controller/TopicsService.java
@@ -2,13 +2,14 @@ package com.example.controller;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.lang.*;
 @Service
 public class TopicsService {
-    List<pojo> topics= Arrays.asList(new pojo(1,"Spring boot","For Microservices"),
-            new pojo(2,"Java","High Level Language"));
+    private List<pojo> topics= new ArrayList<>(Arrays.asList(new pojo(1,"Spring boot","For Microservices"),
+            new pojo(2,"Java","High Level Language")));
 
 
      public List<pojo> getAllTopics()
@@ -20,4 +21,8 @@ public class TopicsService {
      {
          return topics.stream().filter(t->t.getId() == id).findFirst().get();
      }
+
+    public void addTopic(pojo topic) {
+         topics.add(topic);
+    }
 }
